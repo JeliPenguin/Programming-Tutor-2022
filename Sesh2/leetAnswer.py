c8529c7c47ab938e7140091ae5393a567e8f829d
JeliPenguin <jeffbenchu@gmail.com>
2022-10-18 11:10:54 +0100

    Sesh 2

---
+++ Sesh2/leetAnswer.py
@@ -0,0 +1,23 @@
+input = [2, 3, 4, 5, 6]
+target = 6
+
+
+def simple(input, target):
+    for x in range(len(input)):
+        remain = target - input[x]
+        for y in range(len(input)):
+            if input[y] == remain:
+                return ([x, y])
+    return [-1, -1]
+
+
+def upgrade(input, target):
+    tmp = {}
+    for i, num in enumerate(input):
+        if target - num in tmp:
+            return [tmp[target-num], i]
+        tmp[num] = i
+    return [-1, -1]
+
+
+assert(upgrade(input, target) == simple(input, target))
