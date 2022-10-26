63ddb8b750a1b3971d7d2466e642102d215eec4a
JeliPenguin <jeffbenchu@gmail.com>
2022-10-26 15:13:44 +0100

    Added oop

---
+++ Sesh3/OOP.py
@@ -0,0 +1,21 @@
+class Animal():
+    def __init__(self, species, carnivore):
+        self.species = species
+        self.carnivore = carnivore
+
+    def eat(self):
+        if self.carnivore:
+            print("Eating meat")
+        else:
+            print("Eating vegetables")
+
+    def jump(self):
+        print(f"{self.species} is jumping")
+
+
+rabbit = Animal("rabbit", False)
+wolf = Animal("wolf", True)
+animaliList = [rabbit, wolf]
+for ob in animaliList:
+    ob.eat()
+    ob.jump()
